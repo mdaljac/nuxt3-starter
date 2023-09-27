7ee0a8e558654bc089d74ee8c65d4c42579b28c1
Mustafa Daljac <mdaljac@gmail.com>
2023-09-27 19:59:42 +0200

    nuxt-image module added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/device"],
+	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/device", "@nuxt/image"],
 	tailwindcss: {
 		cssPath: "~/assets/css/main.css",
 	},
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,16 @@
 	<div>
 		<h1 class="text-4xl">Home page</h1>
 
-		<div>
+		<div class="mt-10">
 			<p>Device detection:</p>
 			<p>is mobile: {{ $device.isMobile }}</p>
 			<p>is tablet: {{ $device.isTablet }}</p>
 			<p>is desktop: {{ $device.isDesktop }}</p>
+		</div>
+
+		<div class="container mt-10">
+			<p>Nuxt-img component:</p>
+			<NuxtImg src="/static.jpeg" class="w-full lg:w-1/2 mx-auto" />
 		</div>
 	</div>
 </template>
