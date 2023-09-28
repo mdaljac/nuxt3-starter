2290782e1f217d3bd1a7dd0fe128085d479b45b5
Mustafa Daljac <mdaljac@gmail.com>
2023-09-28 12:24:15 +0200

    gsap added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/device", "@nuxt/image"],
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/device",
+		"@nuxt/image",
+		"@hypernym/nuxt-gsap",
+	],
 	tailwindcss: {
 		cssPath: "~/assets/css/main.css",
 	},
